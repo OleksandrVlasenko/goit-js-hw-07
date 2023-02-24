85acfec53d1f64b8c4ba39b8da70c7c076bb40e0
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-02-24 22:57:46 +0200

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const markupForRendering = galleryItems
+	.map(({ preview, original, description }) => {
+		return `<li>
+			<a class="gallery__item" href="${original}">
+				<img class="gallery__image" src="${preview}" alt="${description}"/>
+			</a>
+		</li>`;
+	})
+	.join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markupForRendering);
+
+// Прибираємо нижні падінги у img
+const imgEl = document.querySelectorAll(".gallery img");
+
+imgEl.forEach(elem => (elem.style.display = "block"));
+
+// Створюємо галерею Simplelightbox
+var lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
