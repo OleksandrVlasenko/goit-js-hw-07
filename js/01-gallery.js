a44eb93c5330abaa24228ca4d134596e706dae8a
OleksandrVlasenko <sania.rapsik@gmail.com>
2023-02-24 20:03:42 +0200

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+document.body.style.paddingRight = "calc(20px - (100vw - 100%))";
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const markupForRendering = galleryItems
+	.map(({ preview, original, description }) => {
+		return `<div class="gallery__item">
+				<a class="gallery__link" href="${original}">
+					<img
+						class="gallery__image"
+						src="${preview}"
+						data-source="${original}"
+						alt="${description}"
+					/>
+				</a>
+			</div>`;
+	})
+	.join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markupForRendering);
+
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+galleryEl.addEventListener("click", onOpenModal);
+
+function onOpenModal(event) {
+	event.preventDefault();
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+
+	// Створення модального вікна
+	const {
+		dataset: { source },
+		alt,
+	} = event.target;
+
+	const modal = basicLightbox.create(
+		`<img class="gallery__image" src="${source}" alt="${alt}">`,
+	);
+	modal.show();
+
+	// Заборона прокручування сторінки
+	bodyScrollLock.disableBodyScroll(document.body);
+
+	// Закриття модалки і розблокування скролу сторінки по кліку
+	modal.element().addEventListener("click", onCloseModalByClick);
+
+	function onCloseModalByClick() {
+		modal.close(() => bodyScrollLock.enableBodyScroll(document.body));
+		modal.element().removeEventListener("click", onCloseModalByClick);
+	}
+
+	// Закриття модального вікна і розблокування скролу сторінки по клавіші Escape
+	document.addEventListener("keyup", onCloseModal);
+
+	function onCloseModal(event) {
+		if (event.code === "Escape") {
+			modal.close(() => bodyScrollLock.enableBodyScroll(document.body));
+			document.removeEventListener("keyup", onCloseModal);
+		}
+	}
+}
